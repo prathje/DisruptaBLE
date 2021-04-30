362fa1d8d6516cec41e337ee14ba8ddcdc2c215a
Patrick Rathje <git@patrickrathje.de>
2021-05-01 00:02:18 +0200

    Allocate stack memory directly

--- components/platform/zephyr/hal_task.c
+++ components/platform/zephyr/hal_task.c
@@ -25,20 +25,15 @@ struct zephyr_task *hal_task_create(void (*task_function)(void *), const char *t
         return NULL;
     }
 
+    uint32_t stack_size = task_stack_size + CONFIG_TEST_EXTRA_STACKSIZE;
 
-    // TODO: This is currently based on https://github.com/zephyrproject-rtos/zephyr/issues/26999
-    int32_t ret = k_alloc_thread_stack(task_stack_size, 0, &task->stack);
-
-    if (ret == 0) {
-        // not successful! We need to free the task again
-        k_free(task);
-        return NULL;
-    }
+    // TODO: This is currently based on https://github.com/zephyrproject-rtos/zephyr/issues/26999, use stack allocation api when available
+    task->stack = k_aligned_alloc(Z_KERNEL_STACK_OBJ_ALIGN, Z_KERNEL_STACK_SIZE_ADJUST(stack_size));
 
     task->tid = k_thread_create(
                             &task->thread,
                             task->stack,
-                            task_stack_size + CONFIG_TEST_EXTRA_STACKSIZE,
+                            stack_size,
                             (k_thread_entry_t) task_function,
                             task_parameters,
                             NULL,
