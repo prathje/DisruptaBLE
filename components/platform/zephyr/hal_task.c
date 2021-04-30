2b369b924f590461788cbe99e8e6b8cea759b220
Patrick Rathje <git@patrickrathje.de>
2021-04-30 21:15:09 +0200

    Switched to no-usermode

--- components/platform/zephyr/hal_task.c
+++ components/platform/zephyr/hal_task.c
@@ -9,32 +9,30 @@
 #include "platform/hal_task.h"
 
 #include "ud3tn/common.h"
-
 #include <zephyr.h>
 
-k_tid_t hal_task_create(void (*task_function)(void *), const char *task_name,
+
+struct zephyr_task *hal_task_create(void (*task_function)(void *), const char *task_name,
 		    int task_priority, void *task_parameters,
 		    size_t task_stack_size, void *task_tag)
 {
 	/* ensure that an actual function is provided for thread creation */
 	ASSERT(task_function != NULL);
 
-    k_thread_stack_t *stack;
+    struct zephyr_task *task = k_malloc(sizeof(struct zephyr_task));
 
     // TODO: This is currently based on https://github.com/zephyrproject-rtos/zephyr/issues/26999
-    int32_t ret = k_alloc_thread_stack(task_stack_size, K_USER, &stack);
+    int32_t ret = k_alloc_thread_stack(task_stack_size, 0, &task->stack);
 
     if (ret == 0) {
-        // not successful! We need to free the stack again
+        // not successful! We need to free the task again
+        k_free(task);
         return NULL;
     }
 
-    // we can now allocate memory the thread object
-    struct k_thread *new_thread = k_object_alloc(K_OBJ_THREAD);
-
-    k_tid_t thread_id = k_thread_create(
-                            new_thread,
-                            stack,
+    task->tid = k_thread_create(
+                            &task->thread,
+                            task->stack,
                             task_stack_size + CONFIG_TEST_EXTRA_STACKSIZE,
                             (k_thread_entry_t) task_function,
                             task_parameters,
@@ -45,16 +43,16 @@ k_tid_t hal_task_create(void (*task_function)(void *), const char *task_name,
                             K_NO_WAIT
     );
 
-    if (thread_id == NULL) {
-        k_object_free(stack);
-        k_object_free(new_thread);
+    if (task->tid  == NULL) {
+        k_free(task->stack);
+        k_free(task);
         return NULL;
     }
 
     // TODO: How to name them?
     // TODO: How to tag them?
 
-	return thread_id;
+	return task;
 }
 
 void hal_task_start_scheduler(void)
@@ -62,20 +60,15 @@ void hal_task_start_scheduler(void)
 	// NO scheduler start required for Zephyr
 }
 
-
 void hal_task_delay(int delay)
 {
     k_msleep(delay);
 }
 
 
-void hal_task_delete(k_tid_t task)
+void hal_task_delete(struct zephyr_task *task)
 {
-    struct k_thread *thread = task;
-    k_thread_stack_t *thread_stack = thread->stack_obj;
-
-    k_thread_abort(task); // TODO: Do we need to ensure that the thread is not running anymore?
-
-    k_object_free(thread_stack);
-    k_object_free(thread);
+    k_thread_abort(task->tid); // TODO: Do we need to ensure that the thread is not running anymore?
+    k_free(task->stack);
+    k_free(task);
 }
--- include/platform/zephyr/hal_types.h
+++ include/platform/zephyr/hal_types.h
@@ -3,8 +3,15 @@
 
 #include <kernel.h>
 
+struct zephyr_task {
+    struct k_thread thread;
+    k_tid_t tid;
+    k_thread_stack_t *stack;    // needs to be freed before task is freed!
+};
+
+
 #define QueueIdentifier_t struct k_msgq *
 #define Semaphore_t struct k_sem *
-#define Task_t k_tid_t
+#define Task_t struct zephyr_task*
 
 #endif // HAL_TYPES_H_INCLUDED
