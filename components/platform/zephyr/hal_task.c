57d579d520c67da4f4a0308cb0df6f17bc2d11be
Patrick Rathje <git@patrickrathje.de>
2021-05-28 00:43:58 +0200

    Use correct stack allocation sizes

--- components/platform/zephyr/hal_task.c
+++ components/platform/zephyr/hal_task.c
@@ -16,6 +16,25 @@
 
 #include "platform/hal_heap.h"
 
+
+#ifdef Z_THREAD_STACK_SIZE_ADJUST
+#define HT_STACK_SIZE_ADJUST Z_THREAD_STACK_SIZE_ADJUST
+#else
+#define HT_STACK_SIZE_ADJUST Z_KERNEL_STACK_SIZE_ADJUST
+#endif
+
+#ifdef Z_THREAD_STACK_OBJ_ALIGN
+#define HT_THREAD_STACK_OBJ_ALIGN Z_THREAD_STACK_OBJ_ALIGN
+#else
+#define HT_THREAD_STACK_OBJ_ALIGN Z_KERNEL_STACK_OBJ_ALIGN
+#endif
+
+#ifdef Z_THREAD_STACK_OBJ_ALIGN
+#define HT_THREAD_STACK_OBJ_ALIGN Z_THREAD_STACK_OBJ_ALIGN
+#else
+#define HT_THREAD_STACK_OBJ_ALIGN Z_KERNEL_STACK_OBJ_ALIGN
+#endif
+
 struct zephyr_task *hal_task_create(void (*task_function)(void *), const char *task_name,
 		    int task_priority, void *task_parameters,
 		    size_t task_stack_size, void *task_tag)
@@ -29,10 +48,10 @@ struct zephyr_task *hal_task_create(void (*task_function)(void *), const char *t
         return NULL;
     }
 
-    uint32_t stack_size = task_stack_size + CONFIG_TEST_EXTRA_STACKSIZE;
+    uint32_t adjusted_stack_size = HT_STACK_SIZE_ADJUST(task_stack_size + CONFIG_TEST_EXTRA_STACKSIZE);
 
     // TODO: This is currently based on https://github.com/zephyrproject-rtos/zephyr/issues/26999, use stack allocation api when available
-    task->stack = aligned_alloc(Z_KERNEL_STACK_OBJ_ALIGN, Z_KERNEL_STACK_SIZE_ADJUST(stack_size));
+    task->stack = aligned_alloc(HT_THREAD_STACK_OBJ_ALIGN, adjusted_stack_size);
 
     if (!task->stack) {
         // Failed to allocate enough memory for the stack!
@@ -43,7 +62,7 @@ struct zephyr_task *hal_task_create(void (*task_function)(void *), const char *t
     task->tid = k_thread_create(
                             &task->thread,
                             task->stack,
-                            stack_size,
+                            adjusted_stack_size-K_THREAD_STACK_RESERVED,
                             (k_thread_entry_t) task_function,
                             task_parameters,
                             NULL,
