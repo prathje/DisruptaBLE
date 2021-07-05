fafb509395933153990ea38eeb395958536aa0b2
Patrick Rathje <git@patrickrathje.de>
2021-07-05 11:09:47 +0200

    Allow parallel execution of multiple runs
    Signed-off-by: Patrick Rathje <git@patrickrathje.de>

--- sim/run_group.py
+++ sim/run_group.py
@@ -3,6 +3,8 @@ import subprocess
 import sys
 import itertools
 import re
+import dotenv
+from concurrent.futures import ThreadPoolExecutor
 
 def input_env_to_iter(path):
     if os.path.isfile(path):
@@ -20,7 +22,14 @@ def input_env_to_iter(path):
         print("Could not find file/diretory: " + path)
         exit(1)
 
+
 if __name__ == "__main__":
+
+    config = {
+        **dotenv.dotenv_values(".env"),  # load shared development variables
+        **dotenv.dotenv_values(".env.local"),  # load sensitive variables
+        **os.environ,  # override loaded values with environment variables
+    }
 
     if len(sys.argv) < 2:
         print("Group name required!")
@@ -29,12 +38,25 @@ if __name__ == "__main__":
     group_name = sys.argv[1]
     input_envs = sys.argv[2:]
 
+    num_parallel = 1
+
+    if 'GROUP_PARALLEL_RUNS' in config and int(config['GROUP_PARALLEL_RUNS']) > 1:
+        num_parallel = int(config['GROUP_PARALLEL_RUNS'])
+
+
     iterators = []
     for ie in input_envs:
         iterators.append(input_env_to_iter(ie))
 
-    for args in itertools.product(*iterators):
+    def run_with_args(args):
         run_env = os.environ.copy()
         run_env["SIM_NAME"] = re.sub('[^0-9a-zA-Z]+', '-', group_name + "-" + "-".join(args))
         run_env["SIM_GROUP"] = group_name
         subprocess.run(["python3", "run.py"] + list(args), check=False, env=run_env)  # TODO: Check for errors?
+
+    with ThreadPoolExecutor(max_workers=num_parallel) as executor:
+        try:
+            run_arg_iter = itertools.product(*iterators)
+            futures = executor.map(run_with_args, run_arg_iter)
+        except:
+            executor.shutdown(wait=False, cancel_futures=True)
