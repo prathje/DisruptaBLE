3899c59e55e1240c7f8330d9029dc5d646569de5
Patrick Rathje <git@patrickrathje.de>
2021-07-08 16:46:58 +0200

    Support multiple executions for group runs
    Signed-off-by: Patrick Rathje <git@patrickrathje.de>

--- sim/run_group.py
+++ sim/run_group.py
@@ -4,6 +4,8 @@ import sys
 import itertools
 import re
 import dotenv
+import threading
+import time
 from concurrent.futures import ThreadPoolExecutor
 
 def input_env_to_iter(path):
@@ -43,20 +45,37 @@ if __name__ == "__main__":
     if 'GROUP_PARALLEL_RUNS' in config and int(config['GROUP_PARALLEL_RUNS']) > 1:
         num_parallel = int(config['GROUP_PARALLEL_RUNS'])
 
+    if 'GROUP_NUM_EXECUTIONS' in config and int(config['GROUP_NUM_EXECUTIONS']) > 1:
+        assert 'SIM_RANDOM_SEED' not in config or not config['SIM_RANDOM_SEED']
+        rseed = round(time.time())
+    else:
+        rseed = None
+
 
     iterators = []
     for ie in input_envs:
         iterators.append(input_env_to_iter(ie))
 
+    rseed_lock = threading.Lock()
+
     def run_with_args(args):
+        global rseed
         run_env = os.environ.copy()
         run_env["SIM_NAME"] = re.sub('[^0-9a-zA-Z]+', '-', group_name + "-" + "-".join(args))
         run_env["SIM_GROUP"] = group_name
-        subprocess.run(["python3", "run.py"] + list(args), check=False, env=run_env)  # TODO: Check for errors?
+
+        if rseed is not None:
+            rseed_lock.acquire()
+            run_env["SIM_RANDOM_SEED"] = str(rseed)
+            rseed += 1
+            rseed_lock.release()
+
+        subprocess.run(["python3", "run.py"] + list(args), check=True, env=run_env)  # TODO: Check for errors?
 
     with ThreadPoolExecutor(max_workers=num_parallel) as executor:
         try:
-            run_arg_iter = itertools.product(*iterators)
+            single_run_arg_iter = list(itertools.product(*iterators))
+            run_arg_iter = single_run_arg_iter*int(config['GROUP_NUM_EXECUTIONS'])
             futures = executor.map(run_with_args, run_arg_iter)
         except:
             executor.shutdown(wait=False, cancel_futures=True)
