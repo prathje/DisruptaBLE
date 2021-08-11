8fc8a94567d95f99e4465a5dec2dd005ec108f86
Patrick Rathje <git@patrickrathje.de>
2021-08-11 13:49:19 +0200

    WIP more evaluations
    Signed-off-by: Patrick Rathje <git@patrickrathje.de>

--- sim/export.py
+++ sim/export.py
@@ -107,7 +107,7 @@ def export_pair_timings(db, base_path):
 
     pass
 
-def export_rssi_per_distance(db, base_path):
+def export_rssi_per_distance(db, export_dir):
 
     runs = db((db.run.status == 'processed') & (db.run.group == 'app')).select()
 
@@ -116,7 +116,7 @@ def export_rssi_per_distance(db, base_path):
 
 
     def handle_adv_data(name, data):
-        max_dist = 200
+        max_dist = 500
 
         xs = range(1, max_dist+1)
 
@@ -134,7 +134,6 @@ def export_rssi_per_distance(db, base_path):
 
         def rssi_from_d(d, n, rssi_0):
             ad = -10*n*np.log10(d)+rssi_0
-            print(ad)
             return ad
         finiteYmask = np.isfinite(per_dist_mean)
         ((n, rssi_0), _) = curve_fit(rssi_from_d, np.array(xs)[finiteYmask], np.array(per_dist_mean)[finiteYmask], bounds=([0,-100], [10,0]))
@@ -166,9 +165,8 @@ def export_rssi_per_distance(db, base_path):
             while True:
                 rows = db.executesql('''
                     SELECT adv.rssi as rssi, dp.d + (dp.d_next-dp.d)* ((adv.received_us-dp.us)/(dp.us_next-dp.us)) as d
-                    FROM advertisements adv
+                    FROM (SELECT * FROM advertisements WHERE run = {} LIMIT {} OFFSET {}) as adv
                     JOIN distance_pair dp ON dp.run = adv.run AND dp.device_a = adv.sender AND dp.device_b = adv.receiver AND dp.us = FLOOR(adv.received_us/1000000)*1000000
-                    WHERE adv.run = {} LIMIT {} OFFSET {}
                 '''.format(r.id, limit, offset))
 
                 # rows = db.executesql('''
@@ -197,28 +195,202 @@ def export_rssi_per_distance(db, base_path):
 
         overall_data += data
 
-    handle_adv_data(slugify("RSSI per Distance (overall)"), overall_data)
+        handle_adv_data(slugify("RSSI per Distance (overall) {}".format(r.id)), overall_data)
 
-    # SELECT * FROM distance_pair WHERE device_a = 326 AND device_b = 327 AND us = FLOOR(2028932/1000000)*1000000
+def export_advertisement_reception_rate(db, export_dir):
+    runs = db((db.run.status == 'processed') & (db.run.group == 'app')).select()
+
+    overall_limit = 1000
+    overall_data = []
+
+    def handle_adv_data(name, data):
+        max_dist = 501
+        step = 10
+
+        xs = range(1, max_dist+1, step)
+
+        per_dist = [[] for x in xs]
+
+        for d in data:
+            dist = round(d[-2])
+            if 1 <= dist <= max_dist:
+                index = math.ceil(((dist-1)/step))
+                per_dist[index].append(d[-1])
+
+        per_dist_mean = [np.nanmean(vals)*100 for vals in per_dist]
+        per_dist_lq = [np.nanpercentile(vals, 2.5)*100 for vals in per_dist]
+        per_dist_uq = [np.nanpercentile(vals, 97.5)*100 for vals in per_dist]
+
+        plt.clf()
+        plt.plot(xs, per_dist_mean, linestyle='-', label="Mean", color='C1')
+        plt.fill_between(xs, per_dist_lq, per_dist_uq, color=CONFIDENCE_FILL_COLOR, label='95% Confidence Interval')
+        plt.legend()
+        plt.xlabel("Distance [m]")
+        plt.ylabel("Adv Reception Rate [%]")
+        plt.axis([0, max_dist, 0, 100])
+        plt.grid(True)
+        plt.tight_layout()
+        plt.savefig(export_dir + slugify(name) + ".pdf", format="pdf")
+        plt.close()
+
+
+    for r in runs:
+        name = slugify(("adv reception rate per distance", str(r.name), str(r.id), str(overall_limit)))
+        print("Handling {}".format(name))
+
+        def proc():
+            limit = 20   # batch processing slows down everything right now
+            offset = 0
+
+            data = []
+
+            while True:
+                rows = db.executesql('''
+                    SELECT adv.run, adv.sender, adv.received_us, dp.device_b, dp.d + (dp.d_next-dp.d)* ((adv.received_us-dp.us)/(dp.us_next-dp.us)) as d,
+                    CASE WHEN EXISTS (SELECT * FROM advertisements as ra WHERE ra.run = adv.run AND ra.received_us = adv.received_us AND ra.receiver = dp.device_b)
+                            THEN 1 
+                            ELSE 0
+                        END as received
+                    FROM (SELECT DISTINCT run, sender, received_us, connectable FROM advertisements WHERE run = {} LIMIT {} OFFSET {}) as adv
+                    JOIN distance_pair dp ON dp.run = adv.run AND dp.device_a = adv.sender AND dp.device_b != adv.sender AND dp.us = FLOOR(adv.received_us/1000000)*1000000  
+                '''.format(r.id, limit, offset))
+
+
+                # rows = db.executesql('''
+                #     SELECT adv.rssi as rssi, adv.received_us, pr.pa_x, pr.pa_y, pr.pb_x, pr.pb_y, pr.pc_x, pr.pc_y, pr.pd_x, pr.pd_y
+                #     FROM advertisements adv
+                #     JOIN pos_pair pr ON pr.run = adv.run AND pr.device_a = adv.sender AND pr.device_b = adv.receiver AND pr.us = FLOOR(adv.received_us/1000000)*1000000
+                #     WHERE adv.run = {} LIMIT {} OFFSET {}
+                # '''.format(r.id, limit, offset))
+
+                for row in rows:
+                    data.append(row)
+
+                offset += limit
+
+                if len(rows) < limit or offset >= overall_limit:
+                    break
+
+            return data
+
+        data = cached(name, proc)
+        handle_adv_data(name, data)
+
+        overall_data += data
+
+        handle_adv_data(slugify("Adv reception rate per Distance (overall) {}".format(r.id)), overall_data)
 
 def export_bundle_transmission_success_per_distance(db, base_path):
     '''
 SELECT ROUND(d/5)*5 as de, AVG(end_us IS NOT NULL) FROM (
 
-SELECT bt.*, dp.d + (dp.d_next-dp.d)* ((bt.start_us-dp.us)/(dp.us_next-dp.us)) as d
+
 
 FROM bundle_transmission bt
 JOIN stored_bundle ssb ON ssb.id = bt.source_stored_bundle
 JOIN conn_info ci ON ci.id = bt.conn_info
 JOIN bundle b ON b.id = ssb.bundle AND b.destination_eid = 'dtn://fake' AND b.is_sv = 'F'
 JOIN run r ON r.id = bt.run
-JOIN distance_pair dp ON dp.device_a = ci.client AND dp.device_b = ci.peripheral AND dp.us = FLOOR(bt.start_us/1000000)*1000000
-WHERE r.status = 'finished' LIMIT 10000) as a
-GROUP BY de
+
+WHERE r.status = 'finished' LIMIT 10000)
  '''
     pass
 
 def export_bundle_transmission_time_per_distance(db, base_path):
+
+    runs = db((db.run.status == 'processed') & (db.run.group == 'app')).select()
+
+    for r in runs:
+
+        run_config = json.loads(r.configuration_json)
+        name = slugify(("transmission time per distance", str(r.name), str(r.id)))
+
+
+
+        def proc():
+            bundle_transmission_times = []
+            bundle_transmission_success = [] # TODO?
+
+            data = []
+
+            for ci in db((db.conn_info.run == r)).iterselect():
+
+                if None in [ci.client_channel_up_us, ci.peripheral_channel_up_us, ci.client_channel_down_us, ci.peripheral_channel_down_us]:
+                    continue
+
+                exact_start_us = min(ci.client_channel_up_us, ci.peripheral_channel_up_us)
+                exact_end_us = max(ci.client_channel_down_us, ci.peripheral_channel_down_us)
+
+                start_s = math.floor(exact_start_us/1000000)
+                end_s = math.ceil(exact_end_us/1000000)
+
+                distances = []
+                for s in range(start_s, end_s+1):
+                    dist = (db.executesql(
+                        '''
+                            SELECT dp.us, dp.d + (dp.d_next-dp.d) * (({us}-dp.us)/(dp.us_next-dp.us)) as d
+                            FROM distance_pair dp WHERE dp.device_a = {device_a} AND dp.device_b = {device_b} AND dp.us = FLOOR({us}/1000000)*1000000
+                        '''.format(device_a=ci.client, device_b=ci.peripheral, us=s*1000000)
+                    ))
+                    distances.append(dist[0][1])
+
+                bt_rows = (db.executesql(
+                    '''
+                        SELECT bt.start_us, bt.end_us
+                        FROM bundle_transmission bt
+                        JOIN stored_bundle sb ON sb.id = bt.source_stored_bundle
+                        JOIN bundle b ON b.id = sb.bundle AND b.is_sv = 'F'
+                        WHERE bt.conn_info = {ci_id}
+                        GROUP BY bt.id
+                    '''.format(ci_id=ci.id)
+                ))
+                for bt in bt_rows:
+                    transmission_start_s = math.floor(bt[0]/1000000)
+
+                    if bt[1] is None:
+                        transmission_end_s = transmission_start_s
+                    else:
+                        transmission_end_s = math.floor(bt[1]/1000000)
+
+                    transmission_distances = []
+                    for s in range(transmission_start_s, transmission_end_s+1):
+                        transmission_distances.append(distances[transmission_start_s-start_s])
+                    data.append((bt[0], bt[1], sum(transmission_distances) / len(transmission_distances)))
+
+            return data
+
+        data = cached(name, proc)
+
+        print("ADASDA")
+        print(data)
+
+
+
+            # t_rows = (db.executesql(
+                #     '''
+                #         SELECT bt.start_us, bt.end_us, MAX(dp.d), MIN(dp.d), AVG(dp.d)
+                #         FROM bundle_transmission bt
+                #         JOIN stored_bundle sb ON sb.id = bt.source_stored_bundle
+                #         JOIN bundle b ON b.id = sb.bundle AND b.is_sv = 'F'
+                #         JOIN distance_pair dp ON dp.device_a = {device_a} AND dp.device_b = {device_b} AND dp.us BETWEEN FLOOR(bt.start_us/1000000)*1000000 AND FLOOR(bt.end_us/1000000)*1000000
+                #         WHERE bt.conn_info = {ci_id} AND bt.end_us IS NOT NULL
+                #         GROUP BY bt.id
+                #     '''.format(ci_id=ci.id, device_a=ci.client, device_b=ci.peripheral)
+                # ))
+
+
+
+                # t_rows = (db.executesql(
+                #         '''
+                #             SELECT bt.start_us, bt.end_us, dp.d + (dp.d_next-dp.d) * ((bt.start_us-dp.us)/(dp.us_next-dp.us)) as d
+                #             FROM bundle_transmission bt ON bt.conn_info = ci.id
+                #             JOIN stored_bundle sb ON sb.id = bt.source_stored_bundle
+                #             JOIN bundle b ON b.id = sb.bundle AND b.is_sv = 'F'
+                #             JOIN distance_pair dp ON dp.device_a = ci.client AND dp.device_b = ci.peripheral AND dp.us = FLOOR(bt.start_us/1000000)*1000000
+                #             WHERE ci.id = {}
+                #         '''.format(ci.id, ci.client, ci.peripheral)
+                # ))
+
     '''
 SELECT ROUND(d/5)*5 as de, AVG(end_us-start_us)/1000000 FROM (
 
@@ -235,6 +407,22 @@ GROUP BY de
  '''
     pass
 
+def export_bundle_transmission_time_per_neighbors(db, base_path):
+    '''
+SELECT ROUND(d/5)*5 as de, AVG(end_us-start_us)/1000000 FROM (
+
+SELECT bt.*, dp.d + (dp.d_next-dp.d)* ((bt.start_us-dp.us)/(dp.us_next-dp.us)) as d
+
+FROM bundle_transmission bt
+JOIN stored_bundle ssb ON ssb.id = bt.source_stored_bundle
+JOIN conn_info ci ON ci.id = bt.conn_info
+JOIN bundle b ON b.id = ssb.bundle AND b.b.destination_eid = 'dtn://fake' AND b.is_sv = 'F'
+JOIN run r ON r.id = bt.run
+JOIN distance_pair dp ON dp.device_a = ci.client AND dp.device_b = ci.peripheral AND dp.us = FLOOR(bt.start_us/1000000)*1000000
+WHERE r.status = 'finished') as a
+GROUP BY de
+ '''
+    pass
 
 def export_fake_bundle_propagation_epidemic(db, base_path):
 
@@ -479,7 +667,10 @@ if __name__ == "__main__":
     db.commit() # we need to commit
 
     exports = [
+        export_bundle_transmission_time_per_distance,
+        export_bundle_transmission_time_per_neighbors,
         export_rssi_per_distance,
+        export_advertisement_reception_rate,
         export_fake_bundle_propagation_epidemic,
         export_fake_bundle_propagation_direct,
         export_ict,
